bca74f2d916476cc12f0bde0fab8bae9b0c4c44a
Swastic <swastic.singh@gmail.com>
2022-07-19 22:55:11 +0530

    Add files via upload

---
+++ Assignment 1_python.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Q1: Values:'hello',-87.8,6 
+# Expression: * , - , /, +
+
+# Q2: Variable is defined to store an information whereas string is one type of information.
+
+# Q3: 1.) Numeric: It can store numerical values/information and are of 3 types, integer(stores integer values), float(stores decimal values) and complex number.
+# 2.) Boolean: these are the expressions which results in answers of True or False.
+# 3.) Sequence type: It is an ordered collection of same or different data types. 
+
+# Q4: Expressions are made up of different operators and operands to get the desired results. 
+
+# Q5: An expression can be anything between identifiers, operators (including mathematical operators, boolean operators) etc. in a code. However, a statement can be any line of code. An expression is also part of a statement.
+
+# Q6: 22, as if we want to increase the value by one the correct syntax would be bacon=+1 or bacon = bacon + 1.
+
+# Q7: spamspamspam
+
+# Q8: 100 is an integer whereas Eggs is just a noun which can be used as a variable name. If we use 100 as a varible name then we cannot store 100 in it or there might be other complications.
+
+# Q9: int(),float() & str()
+
+# Q10: In the given statement, 99 is an integer and we can only concatenate same data type. Since others are string it cannot concatenate. We can rectify this by turing 99 into string e.g. str(99) or '99'.
+
+# In[ ]:
+
+
+
+
