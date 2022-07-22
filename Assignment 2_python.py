404db6719dde4ac98763cbdbe9126284bb6bf503
Swastic <swastic.singh@gmail.com>
2022-07-22 22:37:41 +0530

    Add files via upload

---
+++ Assignment 2_python.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Q1: Two values of boolean data type are, TRUE and FALSE.
+
+# Q2: And, or and not are three types of boolean operators.
+
+# Q3: Condition 1(C1) : TRUE ;Condition 2(C2): TRUE== AND --> TRUE , OR --> TRUE & NOT (C1 for eg) --> FALSE
+# C1: TRUE C2: FALSE == AND --> FALSE , OR --> TRUE & NOT (C1 for eg) --> FALSE
+# C1: FALSE C2: FALSE == AND --> FALSE , OR --> FALSE & NOT (C1 for eg) --> TRUE
+# C1: FALSE C2: TRUE == AND --> FALSE , OR --> TRUE & NOT (C1 for eg) --> TRUE
+
+# Q4: FALSE, FALSE, TRUE, FALSE, FALSE, TRUE
+
+# Q5: Less than (<), More than (>), Less than or equal to (<=), More than or equal to (>=), Equal to (==) and Not equal to (!=)
+
+# Q6: In an assignment operator we use single equal sign (=) whereas during comparison we use double (==). E.g.: a = 10 (assignment operator) whereas if a == 10: do something (comparison).
+
+# Q7: spam = 0 ----> (Assignment 1st block)
+# if spam == 10: ----> (Comparison 2nd block)
+#     print("eggs")
+# if spam > 5:----> (Comparison 3rd block)
+#     print("bacon")
+# else:
+#     print("ham")
+# print("spam")
+# print("spam")
+
+# Q8: Spam = 0
+# if Spam == 1:
+#         print("Hello")
+# elif Spam == 2:
+#     print("Howdy")
+# else:
+#     print("Greetings")
+
+# Q9: Stop key and restart the kernal.
+
+# Q10: Break, breaks the loop and goes to the next loop for execution. Whereas, continue starts the next iteration.
+
+# Q11: There no difference between the output of all 3 statements.
+
+# Q12: Program 1(for loop): for i in range(1,11):
+#         print(i)
+#         
+# Program 2(while loop)
+# i=1
+# while i < 11:
+#     print(i)
+#     i+=1
+
+# Q13: spam.bacon()
