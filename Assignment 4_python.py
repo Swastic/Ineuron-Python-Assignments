89e2d8425f8a80b6b94ea63890f22e2c67259c60
Swastic <swastic.singh@gmail.com>
2022-07-31 00:46:48 +0530

    Add files via upload

---
+++ Assignment 4_python.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Q1: [] is one type of bracket, they are typically used to denote programming language constructs such as blocks, function calls or subscripts.
+
+# Q2: spam[2]="hello"
+
+# Q3: 'd'
+
+# Q4: 'd'
+
+# Q5: ['a','b']
+
+# Q6: 1
+
+# Q7: [3.14,'cat',11,'cat',True, 99]
+
+# Q8: [3.14, 11, 'cat', True]
+
+# Q9: List concatenation operator: '+' and List replication operator : '*'
+
+# Q10: List method append(), adds an element at the end of list, whereas insert() can be used to insert any element at whichever position we wish in the given list.
+
+# Q11: pop() and remove().
+
+# Q12: Both are sequences, hence an index is assigned in both which can be used to manipulate as desired.
+
+# Q13: Tuples are immutable objects, lists can be mutated.
+
+# Q14: With parantheses, like a = ('42')
+
+# Q15: By using tuple(name of the list) & by using list(name of the tuple)
+
+# Q16: They contain references to list values.
+
+# Q17: copy.copy() creates a new object then put the reference to the original objects whereas copy.deepcopy() creates a new object then copy the objects from original.
+
+# In[ ]:
+
+
+
+
