53695bd8c4a0622febbff762b97b4e41763636c7
Swastic <swastic.singh@gmail.com>
2022-07-30 22:15:45 +0530

    Add files via upload

---
+++ Assignment 3_python.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Q1: Functions helps to make a program more automated. If you have a program where the work is repeated then its more logical to have a function for that work than type the code again and agian.
+
+# Q2: A function runs only when it is called.
+
+# Q3: An function is created using a keyword called def followed by function name.
+
+# Q4: A function is a program we write for some specific work and when that work is required it is called function call. E.g.: making a screwdriver is like writing a function program while using that screwdriver to unscrew is function call.
+
+# Q5: There is only one global scope, when the program runs it remembers but when we ends or close the program it forgets.A local scope is a varible created inside a function so it can be as many as the defined functions.
+
+# Q6: Local variables are destroyed when the function call returns.
+
+# Q7: Return is basically used to return a value after an statement or function runs.Its like returning a value after the computation. 
+
+# Q8: The return value of a call to function is Void.
+
+# Q9: We can make a function variable refer to the global variable by declaring it as global in the function itself.
+
+# Q10: None is a datatype of itself, NoneType.
+
+# Q11: import areallyourpetsnamederic, import the module areallyourpetsnamederic.
+
+# Q12: sapm.bacon()
+
+# Q13: We can use try and except clause to handle error.
+
+# Q14: Try is used to try execute the code and if it faces an exception the control is given to except block. So, even of there is some problem with the code the program would not crash.
+
+# In[ ]:
+
+
+
+
