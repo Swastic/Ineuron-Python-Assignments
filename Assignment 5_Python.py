5cabd08db2c6e05a860e6f9898f94a08113460dc
Swastic <swastic.singh@gmail.com>
2022-08-13 21:54:34 +0530

    Add files via upload

---
+++ Assignment 5_Python.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Q1:{}
+
+# Q2: a = {'foo':42}
+
+# Q3: List can be accessed using index while in dictionary it can be accesssed using key. Also, order of elements are maintained in list while it is not in dictionary.
+
+# Q4: KeyError would come.
+
+# Q5: No difference as both searches for cat in the keys of spam.
+
+# Q6: 'Cat' in spam searches for 'cat' key in spam while in second one it searches for 'cat' in values of spam.
+
+# Q7: spam.setdefault('color','black')
+
+# Q8: pprint.pprint()
+
+# In[ ]:
+
+
+
+
