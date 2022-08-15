4c5970295d8ee1b45870d7d574d4ed1a668c1253
Swastic <swastic.singh@gmail.com>
2022-08-15 20:20:08 +0530

    Add files via upload

---
+++ Assignment 6_python.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Q1: Escape character is a backslash \ followed by the character you want to escape in a string. It is used to escape the characters you which is illegal to use in python.
+
+# Q2: \n stands for new line and \t stands for horizontal tab.
+
+# Q3: Blackslash characters can be included in the string by using two backslash characters.
+
+# Q4: Single quote in "Howl's Moving Castle" is correct since in the beginning and at the end double quote has been used. If single quote has been used instead of double quote there would have been an error.
+
+# Q5: We can remove \n in the print function itself like print("hjvsh", end = " "), it will just add space rather than newline.
+
+# Q6: 'e', 'Hello','Hello', 'lo World!'
+
+# Q7: 'HELLO', True , 'hello'
+
+# Q8: ['Remember,', 'remember,', 'the', 'fifth', 'of', 'July.'] & 'There-can-only-one.'
+
+# Q9: rjust(len,fillchr),ljust(len,fillchr) and center(len,fillchr)
+
+# Q10: From start: lstrip() and from end: rstrip().
+
+# In[ ]:
+
+
+
+
