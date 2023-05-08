86b3d4c4adf72d2042123c099fdb999aaf325436
Swastic <swastic.singh@gmail.com>
2023-05-08 20:15:34 +0530

    Add files via upload

---
+++ Python_Assignment 9.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Q1: It describes the location of a file relative to the current(working) directory.
+
+# Q2: An absolute path starts from the absolute start of the hard driveand describes every step you must take through the filesystem to end up at the target location.
+
+# Q3: getcwd(): CWD stands for current working directory. This function allows you to see what your current working directory is. chdir(): short for change directory, this function allows you to set current working directory to a path of your choice.
+
+# Q4: The . is current direcrtory while .. signifies the parent directory.
+
+# Q5: C:\bacon\eggs is the directory name and spam.txt is the base name.
+
+# Q6: r: Opens the file in read only mode, rb: Opens the file as read-only in binary format and starts reading from the begining of the file, r+: Opens a file for reading and writing, placing the pointer at the begining of the file.
+
+# Q7: A new file will be opened in the same name in our present directory.
+
+# Q8: The read() will read the whole file at once and then print out the first characters that take up as many bytes as you specify in the parenthesis versus the readline() that will read and print out only the first characters that take up as many bytes as you specify in the parenthesis.
+
+# Q9: A shelf value resembles a dictionary value.
+
+# In[ ]:
+
+
+
+
