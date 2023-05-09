c31087622d81487dc65a3d7164bcc02a3fec7948
Swastic <swastic.singh@gmail.com>
2023-05-09 23:11:51 +0530

    Add files via upload

---
+++ Python_Assignment 13.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Q1: Excel is more superior when compared with the CSV file format; thus, CSV consumes less amount of file size when the user is importing data; it is a much faster format compared to excel. CSV does not manipulate data and stores it as-is. Excel also allows the user the add-in feature.
+
+# Q2: First, call open() and pass it 'w' to open a file in write mode. This will create the object you can then pass to csv. writer() to create a Writer object. On Windows, you'll also need to pass a blank string for the open() function's newline keyword argument.
+
+# Q3:'r'	Open a file for reading. (default) & 'w'	Open a file for writing. Creates a new file if it does not exist or truncates the file if it exists.
+
+# Q4: Using CSV Module
+# We can convert a list to CSV in python easily by importing an in-built CSV module in python. In this method, we use file handling in python to read the data of a list and then write it into a CSV file.
+
+# Q5: This changes the delimiter and line terminator characters in your file. The delimiter is the character that appears between cells on a row. By default, the delimiter for a CSV file is a comma. The line terminator is the character that comes at the end of a row.
+
+# Q6: If you have a JSON string, you can parse it by using the json.loads() method. The result will be a Python dictionary.
+
+# Q7: If you have a Python object, you can convert it into a JSON string by using the json.dumps() method.
+
+# In[ ]:
+
+
+
+
